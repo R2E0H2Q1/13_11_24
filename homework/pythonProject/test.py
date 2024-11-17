22cc97365c236926630b149ce0a5ec6ddd2c8828
R2E0H2Q1 <72799088+R2E0H2Q1@users.noreply.github.com>
2024-11-17 17:56:50 +0200

    Delete homework/pythonProject/test.py

--- homework/pythonProject/test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-#1.1Create a list of 50 random numbers between 1-100
-
-#print(f'1.1. List of random numbers from 1-100: {sorted([random.randint(1, 101) for n in range(50)], key=lambda n: n)}')
-print()
